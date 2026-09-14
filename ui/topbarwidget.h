3b27403522b39082e5f01868a0eed8487d84dc16
Gerd Schendzielorz <gerd.schendzielorz@t-online.de>
2026-09-14 16:49:14 +0200

    D24 - Add telemetry export control to top bar

--- ui/topbarwidget.h
+++ ui/topbarwidget.h
@@ -11,10 +11,17 @@ class TopBarWidget : public QWidget
 public:
     explicit TopBarWidget(QWidget *parent = nullptr);
 
+    /**
+     * @brief Enables or disables telemetry export availability.
+     * @param enabled True if collected simulation telemetry may be exported.
+     */
+    void setExportEnabled(bool enabled);
+
 signals:
     void homeRequested();
     void selectSpacecraftRequested();
     void startSimulationRequested();
+    void exportTelemetryRequested();
     void controlsHelpRequested();
     void coordinateHelpRequested();
     void settingsRequested();
@@ -23,6 +30,7 @@ private:
     QPushButton *homeButton = nullptr;
     QPushButton *selectSpacecraftButton = nullptr;
     QPushButton *startSimulationButton = nullptr;
+    QPushButton *exportTelemetryButton = nullptr;
     QPushButton *controlsHelpButton = nullptr;
     QPushButton *coordinateHelpButton = nullptr;
     QPushButton *settingsButton = nullptr;
