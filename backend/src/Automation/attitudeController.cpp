196a39fa96a52e31e5811436e545f0e77bacfc91
Gerd Schendzielorz <gerd@MacBook-Pro-von-Gerd.local>
2026-09-21 13:26:29 +0200

    D33 - Add tolerance to attitude controller

--- backend/src/Automation/attitudeController.cpp
+++ backend/src/Automation/attitudeController.cpp
@@ -8,10 +8,12 @@ AttitudeController::AttitudeController() : controllerX_(std::make_unique<PD_Cont
 
 Eigen::Vector3d AttitudeController::killRotation(const Eigen::Vector3d& omega, const double& dt) const
 {
+    constexpr double omegaTolerance = 0.005; // rad/s
+
     Eigen::Vector3d rotationCmd{0.0, 0.0, 0.0};
-    rotationCmd.x() = controllerX_->control(0.0, omega.x(), 1.0, 0.0, dt);
-    rotationCmd.y() = controllerY_->control(0.0, omega.y(), 1.0, 0.0, dt);
-    rotationCmd.z() = controllerZ_->control(0.0, omega.z(), 1.0, 0.0, dt);
+    if (std::abs(omega.x()) > omegaTolerance) rotationCmd.x() = controllerX_->control(0.0, omega.x(), 1.0, 0.0, dt);
+    if (std::abs(omega.y()) > omegaTolerance) rotationCmd.y() = controllerY_->control(0.0, omega.y(), 1.0, 0.0, dt);
+    if (std::abs(omega.z()) > omegaTolerance) rotationCmd.z() = controllerZ_->control(0.0, omega.z(), 1.0, 0.0, dt);
 
     return rotationCmd;
 }
