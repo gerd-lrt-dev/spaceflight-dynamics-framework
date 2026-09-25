0121fc0044320252b927e83f585fb67c60f0dacd
Gerd Schendzielorz <gerd.schendzielorz@t-online.de>
2026-09-25 10:31:41 +0200

    D33 - Integrate stabilize controller interface

--- backend/include/Automation/iAttitudeControl.h
+++ backend/include/Automation/iAttitudeControl.h
@@ -10,7 +10,9 @@ public:
 
     virtual Eigen::Vector3d killRotation(const Eigen::Vector3d& omega, const double& dt) const = 0;
 
-    virtual Eigen::Vector3d stabilize(const Eigen::Quaterniond& currentOrientation, const Eigen::Quaterniond& targetOrientation, const Eigen::Vector3d& angularVelocity) const = 0;
+    virtual Eigen::Vector3d stabilize(const Eigen::Quaterniond& currentOrientation, const Eigen::Vector3d& angularVelocity) = 0;
+
+    virtual void deactivateStabilize() = 0;
 };
 
 #endif // IATTITUDECONTROL_H
