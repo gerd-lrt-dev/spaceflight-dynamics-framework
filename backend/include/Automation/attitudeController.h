1b3315bad383bcaa54b5cb9400cb915657134907
Gerd Schendzielorz <gerd.schendzielorz@t-online.de>
2026-09-25 10:31:44 +0200

    D33 - Add stabilize reference state

--- backend/include/Automation/attitudeController.h
+++ backend/include/Automation/attitudeController.h
@@ -11,7 +11,9 @@ public:
 
     Eigen::Vector3d killRotation(const Eigen::Vector3d& omega, const double& dt) const override;
 
-    Eigen::Vector3d stabilize(const Eigen::Quaterniond& currentOrientation, const Eigen::Quaterniond& targetOrientation, const Eigen::Vector3d& angularVelocity) const override;
+    Eigen::Vector3d stabilize(const Eigen::Quaterniond& currentOrientation, const Eigen::Vector3d& angularVelocity) override;
+
+    void deactivateStabilize() override;
 
 
 private:
@@ -20,6 +22,9 @@ private:
     std::unique_ptr<IController> controllerZ_;
 
     std::unique_ptr<IController> stabController_;
+
+    Eigen::Quaterniond targetOrientation_{1.0, 0.0, 0.0, 0.0};
+    bool stabilizeInitialized_ = false;
 };
 
 #endif // ATTITUDECONTROLLER_H
